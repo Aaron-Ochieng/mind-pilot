c972a0ddf5dd986354b17c71ccaba451c1a4dacf
Aaron ochieng <ochiengaoron@gmail.com>
2026-03-20 16:31:57 +0300

    feat(app): build home screen navigation
    - Purpose: load levels and route to Memory and Loop games

---
+++ src/app/index.tsx
@@ -0,0 +1,51 @@
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { useSQLiteContext } from "expo-sqlite";
+import { getAllHydratedLevels } from "@/db/database";
+import useInstructionStore from "@/store/loop-game-instructions";
+
+const Index = () => {
+  const router = useRouter();
+  const db = useSQLiteContext();
+  const { setGamePuzzles, startPos, endPos } = useInstructionStore();
+  useEffect(() => {
+    const loadLevels = async () => {
+      const res = await getAllHydratedLevels(db);
+      setGamePuzzles(res);
+    };
+    void loadLevels();
+  }, [db]);
+  console.log("Start and end position", startPos, endPos);
+  return (
+    <View className="w-full h-full items-center justify-center px-8 bg-slate-900 ">
+      <SafeAreaView />
+      <Pressable
+        onPress={() => router.navigate("/memory")}
+        className="border w-full border-slate-600 dark:border-gray-slate-500 py-4 items-center justify-center rounded-xl"
+      >
+        <Text
+          className="text-white text-xl"
+          style={{ fontFamily: "JetBrainsMono_400Regular" }}
+        >
+          Memory
+        </Text>
+      </Pressable>
+      <Pressable
+        onPress={() => router.navigate("/loop")}
+        className="border mt-4 w-full border-slate-600 dark:border-gray-slate-500 py-4 items-center justify-center rounded-xl"
+      >
+        <Text
+          className="text-white text-xl"
+          style={{ fontFamily: "JetBrainsMono_400Regular" }}
+        >
+          Loop
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Index;
